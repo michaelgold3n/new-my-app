108a2ccc97c44ef66d69fd1422e04bd323e84db0
michael golden <michaelwilliamgolden@gmail.com>
2022-06-20 21:06:15 -0700

    initial

---
+++ map_nft_frontend/pages/index.js
@@ -0,0 +1,34 @@
+import Header from "../components/Header"
+import { useMoralis } from "react-moralis"
+import MapElement from "../components/MapElement"
+import GetAllNFT from "../components/county/GetAllNFT"
+import MintNFT from "../components/county/MintNFT"
+import GetUserNFT from "../components/county/GetUserNFT"
+import Description from "../components/Description"
+
+export default function Home() {
+  const { isWeb3Enabled, account } = useMoralis()
+  
+  return (
+    <div>
+      <Header />
+      {isWeb3Enabled && account ? (
+        <div>
+          <div className="flex flex-col items-center justify-center">
+            <MintNFT />
+            <GetUserNFT />
+          </div>
+        </div>
+      ) : (
+        <div className="font-bold">
+          <GetAllNFT />
+        </div>
+
+      )}
+
+      <div className="flex flex-col items-center justify-center">
+         <Description />
+            </div>
+    </div>
+  )
+}
